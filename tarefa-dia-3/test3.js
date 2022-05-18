be9c90677b48e476d36b95034d2be56704f5a9e4
Fabio Tenorio <fabio.tenorio1@gmail.com>
2022-05-18 20:57:20 -0300

    criação do código JS

---
+++ tarefa-dia-3/test3.js
@@ -0,0 +1,72 @@
+console.log("olá mundo!")
+console.log("meu primeiro programa da Ituring")
+
+// isso é um comentário de uma linha
+
+/* isso é um comentário de várias 
+
+
+linhas */
+// declaraçao de variáveis
+let coisas
+let coisa1, coisa2
+
+// atribuicao de valor a variavel
+coisas = 'quadrado'
+console.log(coisas)
+console.log(coisas,coisa1,coisa2)
+coisa1 = 12
+console.log(coisas,coisa1,coisa2)
+coisa2 = true
+console.log(coisas,coisa1,coisa2)
+
+let nome = 'instrutora jo'
+let sobrenome = 'correa'
+let idade = 38
+let idadeTexto = '38'
+let altura = 1.61
+let logada = true
+let logadaTexto = 'true'
+
+console.log(nome, typeof(nome))
+console.log(sobrenome, typeof(sobrenome))
+console.log(idade)
+console.log(idadeTexto)
+console.log(altura)
+console.log(logada)
+console.log(logadaTexto)
+
+//Array
+let lista = ['arroz', 'feijao', 'batata', nome, 12, true, [1,2,3]]
+console.log(lista, typeof(lista))
+console.log(lista[0])
+console.log(lista[1])
+
+//Objeto popularmente falado
+let mapa = {nome : 'jo', idade: 38}
+console.log(mapa, typeof(mapa))
+console.log(mapa, [nome])
+console.log(mapa.nome)
+
+let nota1 = 8
+let nota2 = 10
+
+let media = (nota1 + nota2) /2
+console.log('media', media)
+
+let resultado = 10 -3
+console.log('rsultado', resultado)
+
+num1 = 1
+num2 = '2'
+resultadoMaluco = num1 + num2
+console.log('resultadoMaluco', resultadoMaluco)
+
+resultadoCorreto = num1 - num2
+console.log(resultadoCorreto)
+
+let idadeJogador = 18
+ if (idadeJogador >= 18){
+    console.log("pode jogar")
+ } else { console.log("n pode jogar")}
+ 
