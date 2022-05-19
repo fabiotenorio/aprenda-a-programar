fc885c146db27c7e17ebef6c68581316983140ad
Fabio Tenorio <fabio.tenorio1@gmail.com>
2022-05-18 21:38:26 -0300

    pratica passagem

--- tarefa-dia-3/pratica-1.js
+++ tarefa-dia-3/pratica-1.js
@@ -3,4 +3,4 @@ let email = 'fabio.tenorio1@gmail.com'
 let profissao = 'Bancário'
 let idade = 41
 let lugarSonho = 'vale do Silicio'
-
+console.log('Olá,',nome, '! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para', lugarSonho,'.Confirma pra gente alguns dados? A sua idade é',idade,'você é',profissao, 'e o seu email é',email,'? Ficamos no aguardo. Muito obrigado e boa viagem! :D')
